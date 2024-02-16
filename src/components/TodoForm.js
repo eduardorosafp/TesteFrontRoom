e83c5a36548c492d6ed0c29ac868272a73c25950
eduardorosafp <eduardorosafp@gmail.com>
2024-02-16 19:51:32 -0300

    adicionando logica, e salvando dados no localstorage

---
+++ src/components/TodoForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    // prevent default action
+    e.preventDefault();
+    if (!value) {
+      // show alert if input is empty
+      alert("O input está vazio!");
+    } else {
+      // add todo
+      addTodo(value);
+      // clear form after submission
+      setValue("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="TodoForm">
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="todo-input"
+        placeholder="What is the task today?"
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
+};
