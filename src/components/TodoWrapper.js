7a80b017879c798ce5076909c27eedaaecd16a5b
eduardorosafp <eduardorosafp@gmail.com>
2024-02-19 12:12:38 -0300

    finalizando entrega

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,43 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Todo } from "./Todo";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
 import { EditTodoForm } from "./EditTodoForm";
+import { api } from "../service/api";
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
 
-  const addTodo = (todo) => {
-    setTodos([
-      ...todos,
-      { id: uuidv4(), task: todo, completed: false, isEditing: false },
-    ]);
+  const addTodo = async (task) => {
+    try {
+      // Adicionar a tarefa localmente
+      const newTodo = {
+        id: uuidv4(),
+        task,
+        completed: false,
+        isEditing: false,
+      };
+      setTodos((prevTodos) => [...prevTodos, newTodo]);
+
+      // Fazer a chamada para o backend
+      const response = await api.post("/tasks", newTodo);
+
+      // Atualizar o estado com a resposta do backend (opcional, dependendo da resposta)
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) => (todo.id === newTodo.id ? response.data : todo))
+      );
+    } catch (error) {
+      console.error("Erro ao adicionar tarefa:", error.message);
+    }
   };
 
-  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+  const deleteTodo = async (id) => {
+    try {
+      // Remover a tarefa localmente
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
 
-  const toggleComplete = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
+      // Fazer a chamada para o backend
+      await api.delete(`/tasks/${id}`);
+    } catch (error) {
+      console.error("Erro ao deletar tarefa:", error.message);
+    }
+  };
+
+  const toggleComplete = async (id) => {
+    try {
+      const response = await api.put(`/tasks/${id}`, {
+        completed: !todos.find((todo) => todo.id === id).completed,
+      });
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) => (todo.id === id ? response.data : todo))
+      );
+    } catch (error) {
+      console.error("Erro ao atualizar a conclusão da tarefa:", error.message);
+    }
   };
 
   const editTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
         todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
       )
     );
   };
 
-  const editTask = (task, id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
-      )
-    );
+  const editTask = async (task, id) => {
+    try {
+      const response = await api.put(`/tasks/${id}`, { task });
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) =>
+          todo.id === id ? { ...response.data, isEditing: false } : todo
+        )
+      );
+    } catch (error) {
+      console.error("Erro ao editar a tarefa:", error.message);
+    }
   };
 
   return (
@@ -47,7 +85,7 @@ export const TodoWrapper = () => {
       {/* display todos */}
       {todos.map((todo) =>
         todo.isEditing ? (
-          <EditTodoForm editTodo={editTask} task={todo} />
+          <EditTodoForm editTodo={editTask} task={todo} key={todo.id} />
         ) : (
           <Todo
             key={todo.id}
